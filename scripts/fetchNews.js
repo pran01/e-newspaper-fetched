67ecef182a3ac408173243c85ec631f69f207f50
Pranav Sinha <pran.sinha1.0@gmail.com>
2023-02-09 14:21:57 +0530

    Push with an intentional CORS issue

--- scripts/fetchNews.js
+++ scripts/fetchNews.js
@@ -5,7 +5,12 @@ const api_key = "pub_1682587aaae7d871ca330b3f32ff492ae3b6b";
 //top= more than 800 characters image doesnt matter
 
 function fetchNews() {
-  fetch(`https://newsdata.io/api/1/news?apikey=${api_key}&language=en`)
+  let headers = new Headers();
+  headers.append("Access-Control-Allow-Origin", "http://localhost:3000");
+  headers.append("Access-Control-Allow-Credentials", "true");
+  fetch(`https://newsdata.io/api/1/news?apikey=${api_key}&language=en`, {
+    headers: headers,
+  })
     .then((res) => res.json())
     .then((data) => data.results)
     .then((results) => {
@@ -32,7 +37,7 @@ function fetchNews() {
         if (
           !result.image_url &&
           result.content.length > 1300 &&
-          result.content.length < 2500 &&
+          result.content.length < 2000 &&
           !sideFound
         ) {
           console.log("side found");
